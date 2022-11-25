9f4d67c76c029be3724a13ecf923aea6fb020d9e
EduardA <akhnazarov1990@gmail.com>
2022-11-25 18:17:34 +0300

    add search

--- sudoku.js
+++ sudoku.js
@@ -5,27 +5,29 @@
  */
 function solve(boardString) {
   let arrPuzzle = [
-    (puzzle_1 = boardString.slice(0, 9).split('')),
-    (puzzle_2 = boardString.slice(9, 18).split('')),
-    (puzzle_3 = boardString.slice(18, 27).split('')),
-    (puzzle_4 = boardString.slice(27, 36).split('')),
-    (puzzle_5 = boardString.slice(36, 45).split('')),
-    (puzzle_6 = boardString.slice(45, 54).split('')),
-    (puzzle_7 = boardString.slice(54, 63).split('')),
-    (puzzle_8 = boardString.slice(63, 72).split('')),
-    (puzzle_9 = boardString.slice(72, 81).split('')),
+    (puzzle_1 = boardString.slice(0, 9).split("")),
+    (puzzle_2 = boardString.slice(9, 18).split("")),
+    (puzzle_3 = boardString.slice(18, 27).split("")),
+    (puzzle_4 = boardString.slice(27, 36).split("")),
+    (puzzle_5 = boardString.slice(36, 45).split("")),
+    (puzzle_6 = boardString.slice(45, 54).split("")),
+    (puzzle_7 = boardString.slice(54, 63).split("")),
+    (puzzle_8 = boardString.slice(63, 72).split("")),
+    (puzzle_9 = boardString.slice(72, 81).split("")),
   ];
-  console.table(arrPuzzle);
-
-  for (let i = 0; i < 8; i += 1) {
-    for (let j = i; j < 9; j++) {}
+  const size = arrPuzzle.length;
+  const box = 3;
+  function tireFound(arrPuzzle) {
+    for (i = 0; i < arrPuzzle.length; i += 1) {
+      for (j = 0; j < arrPuzzle[i].length; j += 1) {
+        if (arrPuzzle[i][j] === "-") {
+          return [i, j];
+        }
+      }
+    }
+    return null;
   }
 }
-// console.log(
-//   solve(
-//     '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
-//   )
-// );
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
